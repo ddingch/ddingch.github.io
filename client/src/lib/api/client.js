c8b3a1b219f129f59173090d8bf39a67b8433f3f
ch_hh <ch_hh@naver.com>
2023-04-21 12:03:41 +0900

    commit

--- client/src/lib/api/client.js
+++ client/src/lib/api/client.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const client = axios.create({
   headers: {
-    Accept: 'text/plain',
-    'Content-Type': 'text/plain',
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
     Cache: 'no-cache',
   },
   baseURL: 'https://port-0-ddingch-2fjdg2blg4kawh5.sel3.cloudtype.app',
